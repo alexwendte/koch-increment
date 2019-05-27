a767fca942a9c1801ba87c2df50263bb9b4be9d5
Alex <alex@wendte.tech>
2019-05-27 02:10:28 -0500

    adds BaseIncrement

---
+++ src/BaseIncrement.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import './Increment.css';
+
+interface IncButtonProps {
+  value: number;
+  className?: string;
+  onClick: (e: any) => void;
+}
+
+const IncButton: React.FC<IncButtonProps> = ({ onClick, value }) => {
+  return (
+    <button className="inc-button" value={value} onClick={onClick}>
+      {value > 0 ? `+ ${value}` : `- ${value * -1}`}
+    </button>
+  );
+};
+
+const BaseIncrement: React.FC<{}> = () => {
+  const [total, setTotal] = React.useState(0);
+
+  const handleButtonClick = (event: React.ChangeEvent<HTMLButtonElement>) => {
+    const { value } = event.currentTarget;
+    const numValue = parseInt(value);
+    setTotal(total + numValue);
+  };
+
+  const handleClear = () => setTotal(0);
+
+  return (
+    <div className="inc-wrapper">
+      <input value={total} readOnly={true} className="inc-input" />
+      <div className="number-buttons">
+        <IncButton value={1} onClick={handleButtonClick} />
+        <IncButton value={5} onClick={handleButtonClick} />
+        <IncButton value={-1} onClick={handleButtonClick} />
+        <IncButton value={-5} onClick={handleButtonClick} />
+      </div>
+      <div className="output-buttons">
+        <button className="inc-button" onClick={handleClear}>
+          C
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default BaseIncrement;
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import 'react-app-polyfill/ie11';
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import BaseIncrement from './BaseIncrement';
 import './reset.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BaseIncrement />, document.getElementById('root'));
